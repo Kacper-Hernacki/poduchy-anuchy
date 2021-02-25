b7e11cfc7ee1bfdb4cdd7c72cc086d49b7ca1ad5
Kacper <hernackikacper@gmail.com>
2021-02-25 17:44:45 +0100

    contact-emailJS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import './Contact.scss';
+import emailjs from 'emailjs-com';
 
 function Contact() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_ap434db',
+        'template_7ao3d9t',
+        e.target,
+        'user_Y91RK2CL5KNadDdb5L9Qo'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
+
   return (
     <div className="contact">
       <h1>Kontakt</h1>
-      <div className="contact__container">
+      <form onSubmit={sendEmail} className="contact__container">
         <div className="contact__row">
           <h3>Imię</h3>
-          <input type="text" placeholder="Imię" />
+          <input type="text" placeholder="Imię" name="name" />
         </div>
         <div className="contact__row">
           <h3>Adres e-mail</h3>
-          <input type="email" placeholder="adres e-mail" />
+          <input type="email" placeholder="adres e-mail" name="email" />
         </div>
         <div className="contact__row">
           <h3>Wiadomość</h3>
@@ -23,8 +45,8 @@ function Contact() {
             id=""
             required></textarea>
         </div>
-        <button>Wyślij</button>
-      </div>
+        <button type="submit">Wyślij</button>
+      </form>
     </div>
   );
 }
